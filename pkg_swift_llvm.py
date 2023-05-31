4261066dd1fb7fe830c9405942c118b3ba3f5cc7
Alex Denisov <alexdenisov@github.com>
2023-05-31 15:40:28 +0200

    Swift: package whole toolchain instead of just resource dir

--- pkg_swift_llvm.py
+++ pkg_swift_llvm.py
@@ -133,13 +133,11 @@ def export_sdk(tgt, swift_source_tree, swift_build_tree):
     shutil.copytree(swift_source_tree / "stdlib" / "public" / "SwiftShims" / "swift" / "shims",
                     tgt / "usr" / "lib" / "swift" / "shims",
                     ignore=shutil.ignore_patterns('CMakeLists.txt'))
-    shutil.copytree(swift_build_tree/ "lib" / "swift",
-                    tgt / "resource-dir")
 
 
-def export_resource_dir(tgt, swift_build_tree):
-    print("assembling resource dir")
-    shutil.copytree(swift_build_tree/ "lib" / "swift", tgt)
+def export_toolchain(tgt, toolchain_dir):
+    print("assembling toolchain")
+    shutil.copytree(toolchain_dir, tgt)
 
 
 def export_stdlibs(exported_dir, swift_build_tree):
@@ -196,6 +194,7 @@ def main(opts):
     os.mkdir(tmp)
     llvm_build_tree = next(opts.build_tree.glob("llvm-*"))
     swift_build_tree = next(opts.build_tree.glob("swift-*"))
+    toolchain_dir = next(opts.build_tree.glob("toolchain-*/codeql-toolchain"))
     earlyswiftsyntax_build_tree = next(opts.build_tree.glob("earlyswiftsyntax-*"))
     configured = configure_dummy_project(tmp, prefixes=[llvm_build_tree, swift_build_tree,
                                                         earlyswiftsyntax_build_tree / "cmake" / "modules"])
@@ -206,7 +205,7 @@ def main(opts):
     export_libs(exported, libs, swift_build_tree)
     export_headers(exported, opts.swift_source_tree, llvm_build_tree, swift_build_tree)
     export_sdk(exported / "sdk", opts.swift_source_tree, swift_build_tree)
-    export_resource_dir(exported / "resource-dir", swift_build_tree)
+    export_toolchain(exported / "toolchain", toolchain_dir)
 
     zip_dir(exported, opts.output)
 
